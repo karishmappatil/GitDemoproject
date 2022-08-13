f95a56c0d4039691a034f929060566ebbf8ee0ad
karishma <karishma@LAPTOP-IN63UK3T>
2022-08-13 17:03:29 +0530

    committing first class

---
+++ src/pack1/SampleGitcode.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class SampleGitcode {
+	
+	public static void m1()
+	{
+		System.out.println("this is git demo");
+	}
+	public static void main(String[] args) {
+		m1();
+	}
+
+}
